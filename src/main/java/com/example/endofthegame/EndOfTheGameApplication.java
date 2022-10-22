e0770e07fba745e52d0c8c29676173ac66220989
Mariusz Pastuszka <mpastuszka@gmail.com>
2022-10-22 09:21:25 +0200

    [init] - project skeleton

---
+++ src/main/java/com/example/endofthegame/EndOfTheGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.endofthegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndOfTheGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndOfTheGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/endofthegame/EndOfTheGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.endofthegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndOfTheGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

3e7f808103ac36b219f0ab37a3b5ccb035767e5b
Mariusz Pastuszka <mpastuszka@gmail.com>
2022-10-23 11:43:34 +0200

    [refactor] - code clean ups

--- src/main/java/com/example/endofthegame/controller/SimpleWebController.java
+++ src/main/java/com/example/endofthegame/controller/SimpleWebController.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// TODO: explain the difference with REST controller
 @Controller
 @Slf4j
 public class SimpleWebController {

8483aad1580bdc42a7ea824e59495fcbc5d6d0c5
Mariusz Pastuszka <mpastuszka@gmail.com>
2022-11-12 12:54:04 +0100

    [change] - endpoint for new incomes implemented - validation - exception handler for MethodArgumentNotValidException

--- src/main/java/ee/secretagency/endofthegame/controller/rest/IncomeAdvisor.java
+++ src/main/java/ee/secretagency/endofthegame/controller/rest/IncomeAdvisor.java
@@ -4,6 +4,7 @@ import ee.secretagency.endofthegame.dto.ErrorInfo;
 import ee.secretagency.endofthegame.exception.IncomeNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,6 +28,20 @@ public class IncomeAdvisor {
                 .timestamp(ZonedDateTime.now())
                 .path(ServletUriComponentsBuilder.fromCurrentRequest().toUriString())
                 //TODO: extract only path
+                .build();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorInfo handleValidationException(MethodArgumentNotValidException exc) {
+        log.warn("validation exception", exc);
+
+        return ErrorInfo.builder()
+                .status(400)
+                .error("validation failed")
+                .message(exc.getMessage())
+                .timestamp(ZonedDateTime.now())
+                .path(ServletUriComponentsBuilder.fromCurrentRequest().toUriString())
                 .build();
     }
 }

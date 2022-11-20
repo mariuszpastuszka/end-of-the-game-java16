3cf2b1bde2574206e4aa7a9f67889990f0275a5c
Mariusz Pastuszka <mpastuszka@gmail.com>
2022-11-20 10:00:17 +0100

    [add] - simple routing for components created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { IncomeListComponent } from './components/income-list/income-list.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {IncomeListComponent} from './components/income-list/income-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import { ExpenseListComponent } from './components/expense-list/expense-list.component';
+import {ExpenseListComponent} from './components/expense-list/expense-list.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -20,9 +21,15 @@ import { ExpenseListComponent } from './components/expense-list/expense-list.com
     BrowserAnimationsModule,
     HttpClientModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterModule.forRoot([
+      {path: '', component: IncomeListComponent},
+      {path: 'income-list', component: IncomeListComponent},
+      {path: 'expense-list', component: ExpenseListComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

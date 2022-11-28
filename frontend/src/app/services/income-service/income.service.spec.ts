707125f9da51fd61be739dacbc13c13f12e9701a
Mariusz Pastuszka <mpastuszka@gmail.com>
2022-11-28 10:25:09 +0100

    [fix] - fixes for hidden field

--- frontend/src/app/services/income-service/income.service.spec.ts
+++ frontend/src/app/services/income-service/income.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { IncomeServiceService } from './income.service';
+import { IncomeService } from './income.service';
 
 describe('IncomeServiceService', () => {
-  let service: IncomeServiceService;
+  let service: IncomeService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(IncomeServiceService);
+    service = TestBed.inject(IncomeService);
   });
 
   it('should be created', () => {
